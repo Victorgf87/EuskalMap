90704493d56e6b4ba75a7abfc0223b389b761e9a
victorgf87 <victor.gf87@gmail.com>
2015-07-26 08:24:19 +0200

    Group class commented

--- app/src/main/java/victorgf87/euskalmap/classes/Group.java
+++ app/src/main/java/victorgf87/euskalmap/classes/Group.java
@@ -4,38 +4,56 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by Nabe on 24/07/15.
+ * Created by Victor Gonzalez on 24/07/15.
+ * This class represents an user group
  */
 public class Group
 {
-    private String groupName;
-    private List<User> users;
+    private String groupName;   //Name of the group
+    private List<User> users;   //List of users that belong to this group
 
-    public Group()
-    {
-        users=new ArrayList<User>();
-    }
 
+
+    /**
+     * Returns list of users.
+     * @return
+     */
     public List<User> getUsers() {
         return users;
     }
 
+    /**
+     * Adds a new user to the list
+     * @param user user to be added
+     */
     public void addUser(User user)
     {
-        if(users==null)users=new ArrayList<User>();
         users.add(user);
     }
 
 
-
-    public Group(String groupName) {
+    /**
+     * Constructor. Creates a new group with given name
+     * @param groupName given name
+     */
+    public Group(String groupName)
+    {
+        users=new ArrayList<User>();
         this.groupName = groupName;
     }
 
+    /**
+     * Returns name of the group.
+     * @return name of the group
+     */
     public String getGroupName() {
         return groupName;
     }
 
+    /**
+     * Method used to build a description text about this object when searching
+     * @return
+     */
     public String resultText()
     {
         return groupName;
